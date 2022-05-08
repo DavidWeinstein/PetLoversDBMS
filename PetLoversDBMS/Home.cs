bccaf0d045fe314512d93647d300425d94487ea9
David Weinstein <weinstein.dev@gmail.com>
2022-05-07 22:01:55 -0400

    init commit

---
+++ PetLoversDBMS/Home.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetLoversDBMS
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PetReport form2 = new PetReport();
+            form2.Show();
+            
+        }
+    }
+}
---
+++ PetLoversDBMS/PetReport.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetLoversDBMS
+{
+    public partial class PetReport : Form
+    {
+        public PetReport()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string scon = "server=localhost;uid=root;pwd=admin123;database=petlovers";
+            using (MySqlConnection cn = new MySqlConnection(scon) )
+            {
+                cn.Open();
+                MySqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = "select * from pet";
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                BindingSource bindingSource = new BindingSource();
+                bindingSource.DataSource = dt;
+                dataGridView1.DataSource = bindingSource;
+            }
+        }
+    }
+}
